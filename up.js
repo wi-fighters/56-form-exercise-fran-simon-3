af968ac2cb3f1d3e04f545922fb6b02d57f6cd70
Simon <simonsch.tz@gmail.com>
2020-01-09 13:16:57 +0100

    Write first function

--- up.js
+++ up.js
@@ -1,1 +1,12 @@
 //activate alternative address
+const checkBox = document.querySelector("#alt-address")
+
+checkBox.addEventListener("click", showForm);
+
+const showForm = () => {
+    const billingForm = document.querySelector("#billing-form")
+    if (checkBox.checked) {
+        console.log(checkBox === checked)
+        billingForm.style.display = "none";
+    }
+} 
